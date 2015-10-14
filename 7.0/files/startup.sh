cdbbd1c8fce49255116b63799fa939afd3ee7641
JeremJR <jeremy.jacquier-roux@bonitasoft.com>
2015-10-14 11:39:42 +0200

    replace sudo by gosu

--- 7.0/files/startup.sh
+++ 7.0/files/startup.sh
@@ -7,7 +7,7 @@ fi
 # ensure to apply the proper configuration
 if [ ! -f /opt/${BONITA_VERSION}-configured ]
 then
-	sudo -u bonita --preserve-env /opt/files/config.sh && touch /opt/${BONITA_VERSION}-configured || exit 1
+	gosu bonita /opt/files/config.sh && touch /opt/${BONITA_VERSION}-configured || exit 1
 fi
 if [ -d /opt/custom-init.d/ ]
 then
@@ -17,4 +17,4 @@ then
 	done
 fi
 # launch tomcat
-exec sudo -u bonita /opt/bonita/BonitaBPMCommunity-${BONITA_VERSION}-Tomcat-7.0.55/bin/catalina.sh run
+exec gosu bonita /opt/bonita/BonitaBPMCommunity-${BONITA_VERSION}-Tomcat-7.0.55/bin/catalina.sh run
