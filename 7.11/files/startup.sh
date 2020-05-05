e3e757412a52609c7c0cd921657c87034c9ae071
DumitruCorini <dumitru.corini@gmail.com>
2020-05-05 16:40:21 +0200

    Prepare 7.11 releases

---
+++ 7.11/files/startup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# ensure to set the proper owner of data volume
+if [ `stat -c %U /opt/bonita/` != 'bonita' ]
+then
+	chown -R bonita:bonita /opt/bonita/
+fi
+# ensure to apply the proper configuration
+if [ ! -f /opt/${BONITA_VERSION}-configured ]
+then
+	gosu bonita /opt/files/config.sh \
+      && touch /opt/${BONITA_VERSION}-configured || exit 1
+fi
+if [ -d /opt/custom-init.d/ ]
+then
+	for f in $(ls -v /opt/custom-init.d/*.sh)
+	do
+		[ -f "$f" ] && . "$f"
+	done
+fi
+# launch tomcat
+export LOGGING_CONFIG="-Djava.util.logging.config.file=${BONITA_SERVER_LOGGING_FILE:-/opt/bonita/BonitaCommunity-${BONITA_VERSION}/server/conf/logging.properties}"
+exec gosu bonita /opt/bonita/BonitaCommunity-${BONITA_VERSION}/server/bin/catalina.sh run
